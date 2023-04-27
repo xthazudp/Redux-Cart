dff8cd955da58330dd9a764172942ba07237733a
Sudip <xthazudp@gmail.com>
2023-04-27 09:19:38 +0545

    setting up store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('root');
+import "./index.css";
+
+import { store } from "./store";
+import { Provider } from "react-redux";
+import App from "./App";
+
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
